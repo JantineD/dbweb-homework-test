c056b73f56e16ad9f4805fc1c244a16d7a3a2b99
Jantine Doornbos <Jantine@MacBook-Pro-van-Jantine.local>
2013-11-29 13:52:18 +0100

    Index toegevoegd

---
+++ index.php
@@ -0,0 +1,10 @@
+<form action=form.php method=get>
+<p>Which option isn't a color?</p>
+<input type="radio" name="group2" value="Rood"> Rood<br>
+<input type="radio" name="group2" value="Groen"> Groen<br>
+<input type="radio" name="group2" value="Blauw"> Blauw<br>
+<input type="radio" name="group2" value="Zwart"> Zwart<br>
+<input type="submit"
+name="submit"
+value="submit">
+</form>
