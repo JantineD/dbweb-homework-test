d7dbf45b6b7f846b0703087d2a5737a44a83b281
Jantine Doornbos <jgdoornbos@gmail.com>
2013-11-29 14:15:39 +0100

    index.php updated

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<form action=form.php method=get>
+<?php
+if (!isset($_REQUEST['submit'])) {
+?>
+<form method=get>
 <p>Which option isn't a color?</p>
-<input type="radio" name="group2" value="Rood"> Rood<br>
-<input type="radio" name="group2" value="Groen"> Groen<br>
-<input type="radio" name="group2" value="Blauw"> Blauw<br>
-<input type="radio" name="group2" value="Zwart"> Zwart<br>
+<input type="radio" name="radio" value="Red"> Red<br>
+<input type="radio" name="radio" value="Green"> Green<br>
+<input type="radio" name="radio" value="Blue"> Blue<br>
+<input type="radio" name="radio" value="Zwart"> Black<br>
 <input type="submit"
 name="submit"
 value="submit">
 </form>
+<?php
+}
+ else {
+if ($_REQUEST['radio'] === "Zwart")
+	{
+	echo "Correct! Black isn't a color.";
+	} else {
+	echo "false" . " " . ($_REQUEST['radio']) . " " . "is a color!";
+	}
+}
+?>
