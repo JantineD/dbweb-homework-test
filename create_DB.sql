e6eb152a3095c9a1bfe08a1f01f9d15d37426eac
Jantine Doornbos <jgdoornbos@gmail.com>
2013-12-05 12:28:57 +0100

    Updated create_DB.sql and added test-data.sql

--- create_DB.sql
+++ create_DB.sql
@@ -1,24 +1,14 @@
---
--- Tabelstructuur voor tabel `choice`
---
-
 CREATE TABLE IF NOT EXISTS `choice` (
   `C_NUMBER` int(11) NOT NULL,
+  `Q_NUMBER` int(11) NOT NULL,
   `C_TEXT` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `CORRECT` int(11) DEFAULT NULL,
   UNIQUE KEY `C_NUMBER` (`C_NUMBER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `question`
---
 
 CREATE TABLE IF NOT EXISTS `question` (
   `Q_NUMBER` int(11) NOT NULL,
   `Q_TEXT` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   UNIQUE KEY `Q_NUMBER` (`Q_NUMBER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
